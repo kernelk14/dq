ba3d6f2f3eefe8e5fd3906fed01cdf56a82c5eed
kernelk14 <khyleisaias@gmail.com>
2022-06-26 21:27:24 +0800

    DQ: Started implementing compilation for dq.
    I'm now starting to find a good language to compile the language
    with.

--- main.py
+++ main.py
@@ -33,133 +33,141 @@ program = [
 #             raise RuntimeError(f"Label `{word}` already defined")
 #         lab[ip] = word
 # while ip < len(program):
-ip = 0
-# while ip < len(program):
-for ip in range(len(program)):
-    code = program[ip]
-    if code.endswith(':'):
-        # print(f"Deque Values: {stack}")
-        assert code.endswith(':')
-        # code = code[:-1]
-        # print(f"label: {code}")
-        if code in lab:
-            raise RuntimeError(f"Label {code} is already defined")
-            # ip += 1
-        code = code[:-1]
-        lab[code] = ip
-        lab_stack.append(lab[code])
-        # calling_lab = lab[code]
-        print(f"Labels: {lab}")
-        print(f"Label Stack: {lab_stack}")
-        ip += 1
-    # print(stack)
-    # print(program)
-    elif code.isdigit():
-        stack.append(code)
-        ip += 1
-        # print(stack)
-    elif code == '+':
-        try:
-            # print(stack)
-            a = stack.pop()
-            b = stack.pop()
-            stack.append(int(a) + int(b))
-        except IndexError as e:
-            print(f"ERROR: Instruction `{code}`, {e}")
-        ip += 1
-        # print(stack)
-    elif code == 'write':
-        tok[ip] = code
-        try:
-            if stack.count == 0:
-                raise IndexError("You are popping from an empty deque.")
-            else:
-                a = stack.pop()
-                print(a)
-        except IndexError:
-            raise RuntimeError("ERROR: You are writing nothing")
-            # exit(1)
-        ip += 1
-        #print(stack)
-    elif code == 'hwrite':
-        try:
-            a = stack.pop()[2:]
-            bo = bytes.fromhex(a)
-            a_s = bo.decode('ASCII')
-            print(a_s)
-        except IndexError:
-            raise RuntimeError("ERROR: You are writing nothing")
-            # exit(1)
-        ip += 1
-    elif code == '-':
-        try:
-            # print(stack)
-            a = stack.pop()
-            b = stack.pop()
-            stack.append(int(a) - int(b))
-        except IndexError as e:
-            print(f"ERROR: Instruction `{code}`, {e}")
-        ip += 1
-    elif code == 'goto':
-        print(f"Label Stack: {lab_stack}")
-        try:
-            a = lab_stack.pop()
-            ip = a
-            print(f"Next label definition is located in pos {ip}")
-            lab_stack.append(a)
-        except IndexError as e:
-            print(f"ERROR: instruction `{code}` in pos {ip}: {e}")
+def interpret(program):
+    ip = 0
+    # while ip < len(program):
+    for ip in range(len(program)):
+        code = program[ip]
+        if code.endswith(':'):
+            # print(f"Deque Values: {stack}")
+            assert code.endswith(':')
+            # code = code[:-1]
+            # print(f"label: {code}")
+            if code in lab:
+                raise RuntimeError(f"Label {code} is already defined")
+                # ip += 1
+            code = code[:-1]
+            lab[code] = ip
+            lab_stack.append(lab[code])
+            # calling_lab = lab[code]
+            print(f"Labels: {lab}")
+            print(f"Label Stack: {lab_stack}")
             ip += 1
-            exit(1)
-        ip += 1
-    elif code == '*':
-        a = stack.pop()
-        b = stack.pop()
-        stack.append(int(a) * int(b))
-        ip += 1
-    elif code.startswith('"'):
-        str_stack.append(program[ip])
-        while not code.endswith('"'):
+        # print(stack)
+        # print(program)
+        elif code.isdigit():
+            stack.append(code)
+            ip += 1
+            # print(stack)
+        elif code == '+':
             try:
-                str_stack.append(program[ip])
-                ip += 1
-                if code.endswith('"'):
-                    a = " ".join(str_stack)
-                    stack.append(a)
+                # print(stack)
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(int(a) + int(b))
+            except IndexError as e:
+                print(f"ERROR: Instruction `{code}`, {e}")
+            ip += 1
+            # print(stack)
+        elif code == 'write':
+            tok[ip] = code
+            try:
+                if stack.count == 0:
+                    raise IndexError("You are popping from an empty deque.")
+                else:
+                    a = stack.pop()
+                    print(a)
             except IndexError:
-                raise RuntimeError("You are calling to an empty deque")
+                raise RuntimeError("ERROR: You are writing nothing")
+                # exit(1)
+            ip += 1
+            #print(stack)
+        elif code == 'hwrite':
+            try:
+                a = stack.pop()[2:]
+                bo = bytes.fromhex(a)
+                a_s = bo.decode('ASCII')
+                print(a_s)
+            except IndexError:
+                raise RuntimeError("ERROR: You are writing nothing")
+                # exit(1)
+            ip += 1
+        elif code == '-':
+            try:
+                # print(stack)
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(int(a) - int(b))
+            except IndexError as e:
+                print(f"ERROR: Instruction `{code}`, {e}")
+            ip += 1
+        elif code == 'goto':
+            print(f"Label Stack: {lab_stack}")
+            try:
+                a = lab_stack.pop()
+                ip = a
+                print(f"Next label definition is located in pos {ip}")
+                lab_stack.append(a)
+            except IndexError as e:
+                print(f"ERROR: instruction `{code}` in pos {ip}: {e}")
+                ip += 1
+                exit(1)
+            ip += 1
+        elif code == '*':
+            a = stack.pop()
+            b = stack.pop()
+            stack.append(int(a) * int(b))
+            ip += 1
+        elif code.startswith('"'):
+            str_stack.append(program[ip])
+            while not code.endswith('"'):
+                try:
+                    str_stack.append(program[ip])
+                    ip += 1
+                    if code.endswith('"'):
+                        a = " ".join(str_stack)
+                        stack.append(a)
+                except IndexError:
+                    raise RuntimeError("You are calling to an empty deque")
+                ip += 1
+            ip += 1
+            # print(tok)
+        elif code.endswith('"'):
+            a = " ".join(str_stack)
+            # a = str_stack.pop()
+            stack.append(a)
+            print("Stack reached here.")
+            print(f"Main Stack: {stack}")
+            ip += 1
+        # Introduce Hexadecimals
+        elif code.startswith('0x'):
+            #print("warning: parsing a hexadecimal.")
+            stack.append(code)
+            ip += 1
+        # print(f"Label IP: {lab[ip]}")
+    
+        elif code.startswith('0b'):
+            stack.append(code)
+            ip += 1
+        elif code == 'drop':
+            stack.pop()
+            ip += 1
+        else:
+            try:
+                if code == lab[code]:
+                    ip += 1
+                    continue
+            except ValueError:
+                stack.append(int(code))
             ip += 1
         ip += 1
-        # print(tok)
-    elif code.endswith('"'):
-        a = " ".join(str_stack)
-        # a = str_stack.pop()
-        stack.append(a)
-        print("Stack reached here.")
-        print(f"Main Stack: {stack}")
-        ip += 1
-    # Introduce Hexadecimals
-    elif code.startswith('0x'):
-        #print("warning: parsing a hexadecimal.")
-        stack.append(code)
-        ip += 1
-    # print(f"Label IP: {lab[ip]}")
 
-    elif code.startswith('0b'):
-        stack.append(code)
-        ip += 1
-    elif code == 'drop':
-        stack.pop()
-        ip += 1
-    else:
-        try:
-            if code == lab[code]:
-                ip += 1
-                continue
-        except ValueError:
-            stack.append(int(code))
-        ip += 1
-    ip += 1
+def com(program):
+    # ip = 0
+    # for ip in range(len(program)):
+    assert False, "Compiling programs not implemented."
+
+interpret(program)
 # while ip < len(program):
 #     word = program[ip]
 #     if word.endswith(':'):
