bda6d87cca75b08344f5eacb94e84923170409aa
kernelk14 <khyleisaias@gmail.com>
2022-06-27 07:29:29 +0800

    DQ: Refine the compilation.

--- main.py
+++ main.py
@@ -168,8 +168,24 @@ def com(program):
     ip = 0
     out.write("#include <iostream>\n")
     out.write("int main(void) {\n")
-    for ip in range(len(program)): 
+    for ip in range(len(program)):
         code = program[ip]
+        if code.endswith(':'):
+            code = code[:-1]
+            for code in lab:
+                raise RuntimeError(f"Label {code} already defined.")
+            lab[code] = ip
+    stack = deque()
+    # while ip < len(program):
+    assert ip < len(program), "The program exceeds the number of instruction."
+    # print(len(program))
+    # print(ip)
+    for ip in range(len(program)): 
+        print(stack)
+        code = program[ip]
+        if code.endswith(':'):
+           ip += 1
+           continue
         if code == 'write':
             a = stack.pop()
             out.write(f"std::cout << {a} << std::endl;\n")
@@ -187,12 +203,44 @@ def com(program):
             b = stack.pop()
             stack.append(a - b)
             print(f"`{code}`: Subtracting objects inside the stack.")
-        else:
-            stack.append(int(code))
-            print(f"`{code}`: Stack Updated.")
-            print(stack)
             ip += 1
-        ip += 1
+        elif code == '*':
+            a = stack.pop()
+            b = stack.pop()
+            stack.append(a * b)
+            ip += 1
+        elif code.startswith('0x'):
+            #print("warning: parsing a hexadecimal.")
+            stack.append(code)
+            ip += 1
+        elif code == 'hwrite':
+            try:
+                print(stack)
+                a = stack.pop()[2:]
+                print(a)
+                bo = bytes.fromhex(a)
+                a_s = bo.decode('ASCII')
+                out.write(f"std::cout << \"{a_s}\" << std::endl;\n")
+            except IndexError:
+                raise RuntimeError("ERROR: You are writing nothing")
+                # exit(1)
+            ip += 1
+        elif code == 'goto':
+            print(f"Goto Stack: {stack}")
+            a = stack.pop()
+            ip = a
+        else:
+            try:
+                stack.append(int(code))
+                print(f"`{code}`: Stack Updated.")
+                print(stack)
+                ip += 1
+            except ValueError:
+                stack.append(lab[code])
+                ip += 1
+            # ip += 1
+        # ip += 1
+    ip += 1
     out.write("}\n")
     # assert False, "Compiling programs not implemented."
 
